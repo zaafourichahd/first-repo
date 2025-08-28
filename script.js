0ea109d51afdfda029af61c441ee548ceb83c685
zaafourichahd <zaafourichahd48@gmail.com>
2025-08-28 02:48:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Choisir un nombre aléatoire entre 1 et 100
 
-let secretNumber = Math.floor(Math.random() * 100) + 1;
+let secretNumber = 21;
 let score = 0;
 function jouer() {
   alert("العب تبدأ! حاول تخمن رقم من 1 إلى 100.");
@@ -10,18 +10,18 @@ function checkGuess() {
   const guess = Number(document.getElementById('guessInput').value);
   const feedback = document.getElementById('feedback');
   if (!guess || guess < 1 || guess > 100) {
-    feedback.textContent = "ادخل رقم بين 1 و 100!";
+    feedback.textContent = "";
     return;
   }
   if (guess === secretNumber) {
-    feedback.textContent = "مبروك! ربحته!";
+    feedback.textContent = "zeyed maak mon héros";
     score += 10;
     document.getElementById('score').textContent = score;
-    secretNumber = Math.floor(Math.random() * 100) + 1; // رقم جديد للعب مرة أخرى
+    secretNumber = 21; // رقم جديد للعب مرة أخرى
   } else if (guess > secretNumber) {
-    feedback.textContent = "نقص شويّ!";
+    feedback.textContent = "nakes chwaya aayech habibi";
   } else {
-    feedback.textContent = "زيد شويّ!";
+    feedback.textContent = "zyd chwaya aayech habibi";
   }
 }
 
